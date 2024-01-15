c3558a276791a5ae9fdb532b8436467b279e4618
Jimena Alexandra Ruiz Cerna <jruiz@novatronic.com>
2024-01-14 22:16:03 -0500

    fix for back deploy

--- demo-power-rangers-back/functions/server.js
+++ demo-power-rangers-back/functions/server.js
@@ -1,16 +1,19 @@
 import express from "express";
 import cors from "cors";
+import serverless from "serverless-http";
 import { getMyTop5PowerRangersSeasons } from "./topSeasons.js";
 
 var app = express();
 app.use(cors());
 
 var port = process.env.PORT || 5000;
+const router = express.Router();
 
-app.get("/my-top-5-power-rangers-seasons", function (req, res) {
+router.get("/my-top-5-power-rangers-seasons", function (req, res) {
   res.json(getMyTop5PowerRangersSeasons());
 });
 
 // Iniciar servidor
-app.listen(port);
-console.log("El servidor se inició correctamente");
+app.use('/.netlify/functions/server', router);
+export const handler = serverless(app);
+// app.listen(port);
